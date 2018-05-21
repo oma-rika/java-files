481bf2ce162be52bc58253cb7d1b576831517bbe
oma-rika <omt2266@gmail.com>
2018-05-21 21:50:24 +0900

    2018/05/21

---
+++ TestDB/src/ChkLoadJdbc.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+
+/**
+ * @author testuser
+ *
+ */
+public class ChkLoadJdbc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+		String msg = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			msg = "ドライバーのロードに成功しました";
+		} catch (ClassNotFoundException e){
+			msg = "ドライバーのロードに失敗しました";
+		}
+		System.out.println(msg);
+	}
+
+}
---
+++ TestDB/src/DBConnector.java
@@ -0,0 +1,47 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+/**
+ *
+ */
+
+/**
+ * @author testuser
+ *
+ */
+
+public class DBConnector {
+	/*
+	 * JDBCドライバー名
+	 */
+	private static String driverName = "com.mysql.jdbc.Driver";
+	/*
+	 * データベース接続URL
+	 */
+	private static String url = "jdbc:mysql://localhost/testdb?autoReconnect=true&useSSL=false";
+	/**
+	 * データベース接続ユーザー名
+	 */
+	private static String user = "root";
+	/**
+	 * データベース接続パスワード
+	 */
+	private static String password = "mysql";
+
+
+	public Connection getConnection(){
+		Connection con = null;
+		try{
+			Class.forName(driverName);
+			con = (Connection)DriverManager.getConnection(url,user,password);
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
+
